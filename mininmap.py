15a3dd4266ec0f6bd92cf11cfdc4c5e13f08413c
Arash Sameni <arashsameniy@gmail.com>
2022-07-29 15:17:54 +0430

    fix reading info.txt

--- mininmap.py
+++ mininmap.py
@@ -1,15 +1,16 @@
 from pkgs.sender_syn_tcp import *
 
 target = input('Target: ')
-start_port, end_port = [int(x) for x in input('Ports range: ').split('-')]
+start_port, end_port = [int(x) for x in input('Port range(1-1000): ').split('-')]
 
-f = open('info.txt', 'r')
-lines = f.readlines()
-src_ip = lines[2]
-src_port = int(lines[3])
-interface = lines[4].strip()
-src_mac = lines[5].strip().replace(' ', '')
-dest_mac = lines[6].strip().replace(' ', '')
+with open('info.txt', 'r') as file:
+    lines = [':'.join(line.split(':')[1:]).strip() for line in file.readlines()]
+    src_ip = lines[2]
+    src_port = int(lines[3])
+    interface = lines[4]
+    src_mac = lines[5].replace(':', '')
+    dest_mac = lines[6].replace(':', '')
+
 for i in range(start_port, end_port + 1):
     send_syn(target, i, src_ip, src_port, dest_mac, src_mac, interface)
     print(f'Sent TCP SYN packet on port {i}')
--- tcp_syn_sender.py
+++ tcp_syn_sender.py
@@ -1,14 +1,14 @@
 from pkgs.pkt_sender import *
 from pkgs.sender_syn_tcp import *
 
-f = open('info.txt', 'r')
-lines = f.readlines()
-dest_ip = lines[0]
-dest_port = int(lines[1])
-src_ip = lines[2]
-src_port = int(lines[3])
-interface = lines[4].strip()
-src_mac = lines[5].strip().replace(' ', '')
-dest_mac = lines[6].strip().replace(' ', '')
-send_syn(dest_ip, dest_port, src_ip, src_port, dest_mac, src_mac, interface)
-print('54 bytes sent on ' + interface)
+with open('info.txt', 'r') as file:
+    lines = [':'.join(line.split(':')[1:]).strip() for line in file.readlines()]
+    dest_ip = lines[0]
+    dest_port = int(lines[1])
+    src_ip = lines[2]
+    src_port = int(lines[3])
+    interface = lines[4]
+    src_mac = lines[5].replace(':', '')
+    dest_mac = lines[6].replace(':', '')
+    send_syn(dest_ip, dest_port, src_ip, src_port, dest_mac, src_mac, interface)
+    print('54 bytes sent on ' + interface)
