d7a990fdadf1e4559cb0d373a97884c994851eb6
Arash Sameni <arashsameniy@gmail.com>
2022-07-29 15:04:36 +0430

    Refactor shell script

--- info.sh
+++ info.sh
@@ -1,8 +1,19 @@
-echo $1 > info.txt
-echo $2 >> info.txt
-ifconfig $3 | awk '/inet / {print $2}' >> info.txt
-echo $4 >> info.txt
-echo $3 >> info.txt
-ip -o link | grep $3 | grep -oP "link\/ether \K\w+(.*?) " | sed 's/:/ /g' >> info.txt
-arp -n | sed -n 2p | awk '{print $3}' | sed 's/:/ /g' >> info.txt
-echo 'bash info.sh 93.184.216.34 80 wlp3s0 3000' >> info.txt
+#!/bin/bash
+
+if [[ $# -eq 4 ]]
+then
+    src_ip=$(ifconfig $3 | awk '/inet / {print $2}')
+    interface_mac_addr=$(ip -o link | grep $3 | grep -oP "link\/ether \K\w+(.*?) ")
+    gateway_mac_addr=$(arp -n | sed -n 2p | awk '{print $3}')
+
+    echo "Dest IP:                 ${1}" > info.txt
+    echo "Dest Port:               ${2}" >> info.txt
+    echo "Src IP:                  ${src_ip}" >> info.txt
+    echo "Src Port:                ${4}" >> info.txt
+    echo "Interface:               ${3}" >> info.txt
+    echo "Interface's Mac Address: ${interface_mac_addr}" >> info.txt
+    echo "Gateway's Mac Address:   ${gateway_mac_addr}" >> info.txt
+else
+    echo "Not enough arguments"
+    echo "Usage: ./info.sh DestIP DestPort Interface SrcPort"
+fi
